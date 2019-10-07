bd38c9c242fe813d81b13f5bdc38158d60502499
Christopher Arndt <chris@chrisarndt.de>
2019-10-07 15:02:48 +0200

    Add a factory preset (param values not accurate)
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- plugins/Juno/Controls.hpp
+++ plugins/Juno/Controls.hpp
@@ -31,6 +31,19 @@ struct Preset {
 };
 
 const Preset factoryPresets[] = {
+    {
+        "JX-3P", {
+            1.0f,           // lv2_enabled
+            lfoTriangle,    // lfo_type
+            0.25f,          // lfo_speed
+            0.75f,          // lfo_amplitude
+            0.0f,           // fast
+            12.0f,          // line_delay
+            0.5f,           // delay_mod_amount
+            -3.0f,          // wet
+            -3.0f           // dry
+        }
+    }
     //,{
     //    "Another preset",  // preset name
     //    {0.5f, ...}        // array of presetCount float param values
